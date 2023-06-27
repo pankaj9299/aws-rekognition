6663f29e297e62902dc5912d15f7f7f9eb06a64e
Pankaj Kumar <pankaj92999@gmail.com>
2023-06-28 02:29:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -82,6 +82,6 @@ app.post('/upload', upload.single('image'), (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", function() {
   console.log('Server is running on port: ' + port);
 });
